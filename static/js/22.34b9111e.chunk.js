df981b0ad429ba188da94ddaee4693212f61866e
Br0kEnM1Nd-Git <122764260+Br0kEnM1Nd-Git@users.noreply.github.com>
2023-12-26 12:47:52 +0000

    Deploying to gh-pages from @ Br0kEnM1Nd-Git/goit-react-hw-08-phonebook@f9322bfe0f6ff2eff0d0441c6cc0cb745bb0fa76 🚀

---
+++ static/js/22.34b9111e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[22],{22:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});const o="Home_homeContainer__Gr0Me";var r=a(184);const s=()=>(0,r.jsx)("div",{className:o,children:"Home"})}}]);
+//# sourceMappingURL=22.34b9111e.chunk.js.map
